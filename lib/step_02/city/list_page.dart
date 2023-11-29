d8efa0e65700ffa5761e619420d19ed6e80448f6
htsuruo <tsuru.dev@gmail.com>
2023-11-29 13:00:51 +0900

    step2

---
+++ lib/step_02/city/list_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'detail_page.dart';
+
+class CityListPage extends StatelessWidget {
+  const CityListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cities = [
+      '札幌市',
+      '横浜市',
+      '川崎市',
+      '名古屋市',
+      '京都市',
+      '大阪市',
+      '堺市',
+      '神戸市',
+      '岡山市',
+      '広島市',
+      '北九州市',
+      '福岡市',
+      '熊本市',
+      '那覇市',
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('市区町村一覧'),
+      ),
+      body: ListView(
+        children: [
+          for (final city in cities)
+            ListTile(
+              title: Text(city),
+              subtitle: const Text('政令指定都市'),
+              trailing: const Icon(Icons.navigate_next),
+              onTap: () {
+                Navigator.of(context).push<void>(
+                  MaterialPageRoute(
+                    builder: (context) => CityDetailPage(city: city),
+                  ),
+                );
+              },
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/step_02/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'city/list_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter RESAS App',
+      home: CityListPage(),
+    );
+  }
+}
