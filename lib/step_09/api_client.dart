fcc4e74d27dfce060c6e1eab9754487f776fd33f
htsuruo <tsuru.dev@gmail.com>
2023-12-09 11:48:48 +0900

    wip: Step9

---
+++ lib/step_09/api_client.dart
@@ -0,0 +1,56 @@
+// ignore_for_file: avoid_classes_with_only_static_members, avoid_dynamic_calls
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../env.dart';
+import 'city/annual_municipality_tax.dart';
+import 'city/city.dart';
+
+abstract class ApiClient {
+  static const _host = 'opendata.resas-portal.go.jp';
+  static final _headers = {
+    'X-API-KEY': Env.resasApiKey,
+  };
+
+  // 市区町村一覧をGETで取得する
+  static Future<List<City>> fetchCities() async {
+    final res = await http.get(
+      Uri.https(_host, '/api/v1/cities'),
+      headers: _headers,
+    );
+    if (res.statusCode == 200) {
+      final json = jsonDecode(res.body)['result'] as List;
+      final items = json.cast<Map<String, dynamic>>();
+      return items.map(City.fromJson).toList();
+    }
+    throw Exception('Failed to load cities');
+  }
+
+  // 一人当たり地方税をGETで取得する
+  static Future<List<AnnualMunicipalityTax>> fetchMunicipalityTaxes({
+    required City city,
+  }) async {
+    final res = await http.get(
+      // 第三引数にパラメータを指定できます
+      Uri.https(_host, '/api/v1/municipality/taxes/perYear', {
+        'prefCode': city.prefCode.toString(),
+        'cityCode': city.cityCode,
+      }),
+      headers: _headers,
+    );
+    if (res.statusCode == 200) {
+      final result = jsonDecode(res.body)['result'] as Map<String, dynamic>;
+      final data = result['data'] as List;
+      final items = data.cast<Map<String, dynamic>>();
+      return items
+          .map(AnnualMunicipalityTax.fromJson)
+          .toList()
+          .reversed
+          .toList();
+    } else {
+      throw Exception('Failed to load municipality taxes');
+    }
+  }
+}
---
+++ lib/step_09/city/list_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+import '../api_client.dart';
+import '../widgets/centered_circular_progress_indicator.dart';
+import '../widgets/centered_error_text.dart';
+import 'city.dart';
+import 'detail_page.dart';
+
+class CityListPage extends StatefulWidget {
+  const CityListPage({super.key});
+
+  @override
+  State<CityListPage> createState() => _CityListPageState();
+}
+
+class _CityListPageState extends State<CityListPage> {
+  late Future<List<City>> _citiesFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _citiesFuture = ApiClient.fetchCities();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('市区町村一覧'),
+      ),
+      body: FutureBuilder<List<City>>(
+        future: _citiesFuture,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                return CenteredErrorText(error: snapshot.error!);
+              }
+              // 本来はhasErrorで分岐するべきですが割愛
+              final cities = snapshot.data!;
+              return ListView.builder(
+                itemCount: cities.length,
+                itemBuilder: (context, index) {
+                  final city = cities[index];
+                  return ListTile(
+                    title: Text(city.cityName),
+                    subtitle: Text(city.cityType.label),
+                    trailing: const Icon(Icons.navigate_next),
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(
+                            city: city,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              );
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+            case ConnectionState.active:
+          }
+          return const CenteredCircularProgressIndicator();
+        },
+      ),
+    );
+  }
+}
